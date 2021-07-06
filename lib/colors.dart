499d42020911c9c839a7402646811c9420ea9334
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-07-06 16:00:22 +0200

    Modified

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,5 +1,8 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
 class AppColor{
-
+  static const Color orange = Color(0xFFFB9800);
+  static const Color black = Color(0xFF000000);
+  static const Color gray = Color(0xFF919191);
+  static const Color white = Color(0xFFFFFFFF);
 }

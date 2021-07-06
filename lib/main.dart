a40c987d7e28c01cd8b92a6160a5c9735d829a5e
AhmedShawkyAhmed <shawkyahmed392@gmail.com>
2021-07-06 16:00:27 +0200

    Modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:shahen/stateful/rate.dart';
+import 'package:sizer/sizer.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,10 +9,14 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Google Maps Demo',
-      home: DriverRate(),
+    return Sizer(
+        builder: (context, orientation, deviceType) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Google Maps Demo',
+            home: DriverRate(),
+          );
+        }
     );
   }
 }

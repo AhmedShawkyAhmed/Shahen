e64210b52cc5d6bef55f5fe72edb3ab8fe4f4822
mohamedhani1997 <45540581+mohamedhani1997@users.noreply.github.com>
2021-07-12 17:04:15 +0200

    Update colors.dart

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,4 +4,11 @@ class AppColor{
   static const Color black = Color(0xFF000000);
   static const Color gray = Color(0xFF747474);
   static const Color white = Color(0xFFFFFFFF);
+  static const Color darkBlue =Color(0xFF111829);
+  static const Color green =Color(0xFF18BE18);
+  static const Color lightGray =Color(0xFF8F8F8F);
+  static const Color black =Color(0xFF000000);
+  static  IconThemeData lightGrayIcon =IconThemeData(color: Color(0xFF8F8F8F)) ;
+  static  IconThemeData orangeIcon =IconThemeData(color: Color(0xFFFB9800)) ;
+  static  IconThemeData grayIcon =IconThemeData(color: Color(0xFF333333)) ;
 }
